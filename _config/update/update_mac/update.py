ba1ca1dfacef738ae98e623c7330b982456f1405
Christoph <c.weberndorfer@gmail.com>
2025-10-28 10:23:05 +0100

    New Update (Administrator) -

--- _config/update/update_mac/update.py
+++ _config/update/update_mac/update.py
@@ -3,56 +3,38 @@
 
 import os
 import sys
-from time import sleep
 import subprocess
-import shutil
+import requests
+from pathlib import Path
 
 path_programm=os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(sys.argv[0]))))))
-#print(path_programm)
 #print(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(sys.argv[0])))))
 
 
 if sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
-    if path_programm is '':
+    if path_programm == '':
         path_programm = "."
 
 print('Programm wird aktualisiert...')
+print("""
+Liebe:r LaMA-Nutzer:in,
+aufgrund einer größeren strukturellen Änderung bei der Installation von
+LaMA, muss das Programm neu installiert werden.
+Dazu wird auch die Aufgaben-Datenbank noch einmal neu heruntergeladen.
+LaMA ist nach erfolgreicher Installation unter "Programme" zu finden. 
+Alle bisherigen Ordner von LaMA werden nicht mehr benötigt und können 
+anschließend gelöscht werden. 
 
-opened_file=os.path.basename(sys.argv[0])
+Die Neuinstallation passiert großteils automatisch, 
+kann jedoch mehrere Minuten in Anspruch nehmen. 
+Wir bitten um Verständnis!
 
-### Delete Teildokument Folder to force update of log_files ###
-dir_teildokument=os.path.join(path_programm,'Teildokument')
-if os.path.isdir(dir_teildokument):
-    try:
-        for files in os.listdir(dir_teildokument):
-            file_path=os.path.join(dir_teildokument, files)
-            if files.startswith('log_file_') or  files.startswith('Schularbeit_Vorschau') or files.startswith('Teildokument_'):
-                if os.path.splitext(files)[1] != ".pdf":
-               
-                    os.unlink(file_path)
-    except PermissionError:
-        print('Das Update kann nicht durchgeführt werden, da der Ordner Teildokument von einem anderen Prozess verwendet wird.')
-        input()
-        quit()
-else:
-    pass
+Sollten Probleme auftreten, bitte kontaktieren Sie uns unter lama.helpme@gmail.com
 
+""")
+      
+rsp = input("Soll die Neuinstallation von LaMA gestartet werden? (j/n)")
 
-
-name, extension=os.path.splitext(opened_file)
-
-#updatefile_path=os.path.join(path_programm,'_database','_config','update','update%s'%extension)
-if sys.platform.startswith('linux'):
-    folder='update_linux'
-elif sys.platform.startswith('darwin'):
-    folder='update_mac'
-else:
-    folder='update_windows'
-    
-newapp_path=os.path.join(path_programm,'LaMA_programdata','_database','_config','update',folder,'LaMA%s'%extension)
-mainfile_path=os.path.join(path_programm,'LaMA%s'%extension)
-# print(newapp_path)
-# print(mainfile_path)
 def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filledLength = int(length * iteration // total)
@@ -62,45 +44,85 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
     if iteration == total: 
         print()
 
-items = list(range(0, 100))
-l = len(items)
+if rsp=="j":
+    print("Neue Version von LaMA wird heruntergeladen ...")
 
-# Initial call to print 0% progress
-printProgressBar(0, l, prefix = 'Installation:', length = 50)
-for i, item in enumerate(items):
-    if i==50:
-        if sys.platform.startswith('linux'):
-            p=subprocess.Popen('cp "{0}" "{1}"'.format(newapp_path, mainfile_path), stdout=subprocess.PIPE,shell=True)
-        elif sys.platform.startswith('darwin'):
+    if sys.platform.startswith("darwin") or sys.platform.startswith("linux"):
+        path_installer = os.path.join(
+            Path.home(), "Downloads", "LaMA_setup.dmg"
+        )
+
+    opened_file=os.path.basename(sys.argv[0])
+    name, extension=os.path.splitext(opened_file)
+
+    # newapp_path=os.path.join(path_programm,'LaMA_programdata','_database','_config','update','update_mac','LaMA%s'%extension)
+    # mainfile_path=os.path.join(path_programm,'LaMA%s'%extension)
+
+    download_link = (
+        "https://github.com/mylama/lama/releases/latest/download/LaMA_setup.dmg"
+    )
+
+    try:
+        with requests.get(download_link, stream=True) as r:
+            r.raise_for_status()
+            total_size = int(r.headers.get('content-length', 0))
+            block_size = 102400  # 1 KB Blöcke
+            downloaded = 0
+            items = list(range(0, total_size))
+            l = len(items)
+            printProgressBar(0, l, prefix = 'Download:', length = 50)
+            with open(path_installer, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=block_size):
+                    if chunk:
+                        f.write(chunk)
+                        downloaded += len(chunk)
+                        percent = int(downloaded * 100 / total_size)
+                        printProgressBar(downloaded, l, prefix = 'Download:',  length = 50)
+            print("LaMA wurde erfolgreich heruntergeladen.")
+        if sys.platform.startswith('darwin'):  # macOS
+            subprocess.call(['open', path_installer])
+        os._exit(0)
+    except requests.exceptions.ConnectionError:
+        print('Das Programm konnte nicht aktualisiert werden. Stellen Sie sicher, dass eine stabile Internetverbindung besteht und versuchen Sie es erneut.' )
+        os._exit(0)
+
+
+
+
+# for i, item in enumerate(items):
+#     if i==50:
+#         if sys.platform.startswith('linux'):
+#             p=subprocess.Popen('cp "{0}" "{1}"'.format(newapp_path, mainfile_path), stdout=subprocess.PIPE,shell=True)
+#         elif sys.platform.startswith('darwin'):
             
-            p=subprocess.Popen('cp "{0}" "{1}"'.format(newapp_path, mainfile_path), stdout=subprocess.PIPE,shell=True)
-        else:
-            p=subprocess.Popen('copy "{0}" "LaMA{1}"'.format(newapp_path, extension), stdout=subprocess.PIPE,shell=True)
+#             p=subprocess.Popen('cp "{0}" "{1}"'.format(newapp_path, mainfile_path), stdout=subprocess.PIPE,shell=True)
+#         else:
+#             p=subprocess.Popen('copy "{0}" "LaMA{1}"'.format(newapp_path, extension), stdout=subprocess.PIPE,shell=True)
 
-        (output, err) = p.communicate()
-        p_status=p.wait()
+#         (output, err) = p.communicate()
+#         p_status=p.wait()
 
-    sleep(0.01)
-    # Update Progress Bar
-    printProgressBar(i + 1, l, prefix = 'Installation:',  length = 50)
+#     sleep(0.01)
+#     # Update Progress Bar
+#     printProgressBar(i + 1, l, prefix = 'Installation:',  length = 50)
 
-if p_status==0:
-    print('\nProgramm wurde erfolgreich aktualisiert. Drücken Sie "Enter", um fortzufahren...')
-else:
-    #print(newapp_path)
-    print('\nProgramm konnte nicht aktualisiert werden. Bitte versuchen Sie es später erneut.\nFehler: "%s"\n\nDrücken Sie "Enter", um mit der älteren Version fortzufahren...'%str(output)[2:-5]) 
+# if p_status==0:
+#     print('\nProgramm wurde erfolgreich aktualisiert. Drücken Sie "Enter", um fortzufahren...')
+# else:
+#     #print(newapp_path)
+#     print('\nProgramm konnte nicht aktualisiert werden. Bitte versuchen Sie es später erneut.\nFehler: "%s"\n\nDrücken Sie "Enter", um mit der älteren Version fortzufahren...'%str(output)[2:-5]) 
     
-input()
+# input()
 
-if sys.platform.startswith('linux'):
-    if extension=='.py':
-        subprocess.run("python3 " + mainfile_path, shell=True)
-    else:
-        subprocess.run(mainfile_path, shell=True)
-elif sys.platform.startswith('darwin'):
-    os.system(mainfile_path)
-    #subprocess.run("python3 " + mainfile_path, shell=True)
-else:
-    os.startfile(mainfile_path)
+# if sys.platform.startswith('linux'):
+#     if extension=='.py':
+#         subprocess.run("python3 " + mainfile_path, shell=True)
+#     else:
+#         subprocess.run(mainfile_path, shell=True)
+# elif sys.platform.startswith('darwin'):
+#     os.system(mainfile_path)
+#     #subprocess.run("python3 " + mainfile_path, shell=True)
+# else:
+#     os.startfile(mainfile_path)
  
 
